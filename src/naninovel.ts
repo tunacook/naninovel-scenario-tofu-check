f03c1e0a8a2da23761f9697787525ea669bf1a08
tunacook <tunacook0704@gmail.com>
2025-01-06 13:43:18 +0900

    Fix lint

--- src/naninovel.ts
+++ src/naninovel.ts
@@ -3,7 +3,7 @@
  * @param line
  */
 function isCommandLine(line: string): boolean {
-  return line.trimStart().startsWith('@');
+  return line.trimStart().startsWith('@')
 }
 
 /**
@@ -11,7 +11,7 @@ function isCommandLine(line: string): boolean {
  * @param line
  */
 function isCommentLine(line: string): boolean {
-  return line.trimStart().startsWith(';');
+  return line.trimStart().startsWith(';')
 }
 
 // TODO: ローカライズ対応、IDをスキップする https://naninovel.com/ja/guide/localization#%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%A9%E3%82%A4%E3%82%B9%E3%82%99
@@ -20,6 +20,6 @@ function isCommentLine(line: string): boolean {
  * Naninovelの構文であるかどうか Naninovel構文であればスキップする
  */
 export function isSkipNaninovelSyntax(line: string): boolean {
-  if (isCommandLine(line)) return true;
-  return isCommentLine(line);
+  if (isCommandLine(line)) return true
+  return isCommentLine(line)
 }
